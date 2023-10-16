a985a4621402da203fd690aa4785cc9f240ec282
Hussein Medhat <medhathussein2@gmail.com>
2023-10-16 22:05:23 +0300

    Added 3 stats to control the modals appearance

--- src/components/PostHeader/PostHeader.js
+++ src/components/PostHeader/PostHeader.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import OptionsCard from "../OptionsCard/OptionsCard";
 import ProfileCard from "../ProfileCard/ProfileCard";
 import RedditCard from "../RedditCard/RedditCard";
 import "./PostHeader.scss";
 
 export default function PostHeader() {
+  const [profileCardActive, setProfileCardActive] = useState(false);
+  const [redditCardActive, setRedditCardActive] = useState(false);
+  const [optionsCardActive, setOptionsCardActive] = useState(false);
   return (
     <div className="post-header">
       <div className="post-header--left">
@@ -13,21 +17,36 @@ export default function PostHeader() {
           className="post-header__profile-pic"
         />
         <p>
-          <a className="post-header__reddit" href="#">
+          <a
+            className="post-header__reddit"
+            href="#"
+            onMouseEnter={() => setRedditCardActive(true)}
+            onMouseLeave={() => setRedditCardActive(false)}
+          >
             r/anime
+            {redditCardActive && <RedditCard />}
           </a>
           <span className="post-header__date"> • 3 mo. ago</span>
         </p>
-        <a className="post-header__username" href="#">
+        <a
+          className="post-header__username"
+          href="#"
+          onMouseEnter={() => setProfileCardActive(true)}
+          onMouseLeave={() => setProfileCardActive(false)}
+        >
           Turbostrider27
+          {profileCardActive && <ProfileCard />}
         </a>
-        {/* <ProfileCard /> */}
-        {/* <RedditCard /> */}
       </div>
       <div className="post-header--right">
         <button className="post-header__btn">Join</button>
-        <button className="post-header__options">•••</button>
-        {/* <OptionsCard /> */}
+        <button
+          className="post-header__options"
+          onClick={() => setOptionsCardActive((curState) => !curState)}
+        >
+          •••
+        </button>
+        {optionsCardActive && <OptionsCard />}
       </div>
     </div>
   );
