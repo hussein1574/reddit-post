2955b3f4761bdf0c02c1bc46814df593cf6c0bc0
Hussein Medhat <medhathussein2@gmail.com>
2023-10-21 01:29:09 +0300

    Added partial support for tablets and mobile phones

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -147,10 +147,9 @@ export default function Comments({ comments, onComment }) {
             <svg
               rpl=""
               fill="currentColor"
-              height="20"
+              className="comments__add__btn__icon"
               icon-name="add-outline"
               viewBox="0 0 20 20"
-              width="20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M19 9.375h-8.375V1h-1.25v8.375H1v1.25h8.375V19h1.25v-8.375H19v-1.25Z"></path>
